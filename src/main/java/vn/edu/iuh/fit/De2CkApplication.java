142f93826f3447f474c0695abb4828792443cf17
ThuyTien2111 <vanbac21112k2@gmail.com>
2023-11-30 21:53:11 +0700

    done de2

---
+++ src/main/java/vn/edu/iuh/fit/De2CkApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.de2ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class De2CkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(De2CkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/de2ck/De2CkApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.de2ck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class De2CkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
